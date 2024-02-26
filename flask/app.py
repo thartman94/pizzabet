369ddf3b74c1664a6b3eced68cf8de81d127f57b
Tom Hartman <68175233+thartman94@users.noreply.github.com>
2024-02-25 16:05:05 -0800

    finished basic models (#1)

--- flask/app.py
+++ flask/app.py
@@ -14,5 +14,6 @@ app.register_blueprint(api, url_prefix="/api")
 if __name__ == '__main__':
     db.init_app(app)
     with app.app_context():
+      db.drop_all()
       db.create_all()
     app.run(debug=True, host='0.0.0.0', port=4000)
--- flask/models/payment.py
+++ flask/models/payment.py
@@ -1,17 +1,23 @@
 from . import db
 
-class Bet(db.Model):
+class Payment(db.Model):
     __tablename__ = "payments"
 
     id = db.Column(db.Integer, primary_key=True)
     pizza_count_paid = db.Column(db.Integer, nullable=False)
-    # bet_id = db.Column(db.Integer, db.ForeignKey('bet.id'))
-    
+    bet_id = db.Column(db.Integer, db.ForeignKey('bet.id'), nullable=False)
+    accepted = db.Column(db.Boolean, nullable=False, default=False)
 
-    def __init__(self, user_a: int, user_b: int, witnesses: list[int]):
-        self.user_a = user_a
-        self.user_b = user_b
-        self.witnesses = witnesses
-        self.is_settled = False
+    def __init__(self, pizza_count_paid, bet_id, accepted=False):
+      self.pizza_count_paid = pizza_count_paid
+      self.bet_id = bet_id
+      self.accepted = accepted
+        
+    def json(self):
+      return {
+        "pizza_count_paid" : self.pizza_count_paid,
+        "bet_id" : self.bet_id,
+        "accepted" : self.accepted
+      }
 
 
